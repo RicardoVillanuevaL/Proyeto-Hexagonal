96e0cd305195ee414f5c3092e4290a8501a81ecc
RicardoVillanuevaL <50005284+RicardoVillanuevaL@users.noreply.github.com>
2020-08-14 13:14:26 -0500

    creo que ya esta todo T_T

--- services/ProductServices.cs
+++ services/ProductServices.cs
@@ -3,24 +3,53 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using RestSharp;
+using System.IO;
+using System.Web;
 
 namespace services
 {
     public class ProductServices
     {
-        public void registrarProducto(Producto producto)
+        public dynamic registrarProducto(Producto producto)
         {
-            JsonConvert.SerializeObject(producto, Formatting.Indented);
+            string jsonconvertProdcuto = JsonConvert.SerializeObject(producto);
+            string url = "https://api-pruebas2020.herokuapp.com/Producto/registroProducto";
+            try
+            {
+                var client = new RestClient(url);
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                request.AddParameter("application/json", jsonconvertProdcuto,ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+
+                dynamic datos = JsonConvert.DeserializeObject(response.Content);
+
+                return datos;
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
         }
         public List<Producto> consultaProductos()
         {
             List<Producto> lista = new List<Producto>();
             string url = "https://api-pruebas2020.herokuapp.com/Producto/ConsultarProducto";
-            var jsonResult = new WebClient().DownloadString(url);
-            dynamic m = JsonConvert.DeserializeObject(jsonResult);
-            foreach(var i in m)
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+            httpWebRequest.Credentials = CredentialCache.DefaultCredentials;
+            httpWebRequest.Proxy = null;
+            HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            string data = HttpUtility.HtmlDecode(streamReader.ReadToEnd());
+            dynamic listdata = JsonConvert.DeserializeObject(data);
+            foreach(var i in listdata)
             {
-                Producto temp = JsonConvert.DeserializeObject<Producto>(i);
+                Producto temp = JsonConvert.DeserializeObject(i);
                 lista.Add(temp);
             }
             return lista;
